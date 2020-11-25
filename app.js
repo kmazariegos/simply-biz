857021ab129aaa18a8bfebb90dcb54aa8b29e7ff
Karla <mazariek@gmail.com>
2020-11-25 14:50:36 -0800

    First commit

---
+++ app.js
@@ -0,0 +1,94 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const cors = require("cors");
+const passport = require("passport");
+const passportLocal = require("passport-local").Strategy;  
+const cookieParser = require("cookie-parser");
+const bcrypt = require("bcryptjs");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const app = express();
+const User = require("./user");
+
+mongoose.connect('mongodb+srv://mazariek:mazariek@cluster0.r14i9.mongodb.net/test?retryWrites=true&w=majority', 
+{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}, ()=>{
+    console.log("Mongoose is Connected")
+})
+
+//Middleware 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+    methods: ['GET', 'POST']
+  })
+);
+
+app.use(
+  session({
+    secret: "secretcode",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.use(cookieParser("secretcode"));
+app.use(passport.initialize());
+app.use(passport.session());
+require("./passportConfig")(passport);
+
+
+//ROUTES
+app.post("/login", (req, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
+    if (err) throw err;
+    if (!user) res.send("No User Exists");
+    else {
+      req.logIn(user, (err) => {
+        if (err) throw err;
+        res.send("Successfully Authenticated");
+        console.log(req.user);
+      });
+    }
+  })(req, res, next);
+});
+
+app.post("/register", (req, res) => {  
+  User.findOne({ username: req.body.username }, async (err, doc) => {
+    if (err) { throw err };
+    if (doc) { res.send("User Already Exists")};
+    if (!doc) {
+      const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+      const newUser = new User({
+        username: req.body.username,
+        password: hashedPassword,
+      });
+      await newUser.save();
+      res.send("User Created"); 
+    }
+  });
+});
+
+
+app.get("/user", (req, res) => {
+  res.send(req.user);
+});
+
+
+
+app.listen(5000, () => {
+    console.log("Server Has Started");
+  });
+
+
+//KEEEEEEP THISSSS
+//**** npm run dev  
+//in order to run your server
+// then you need to cd client
+//then you *** npm start
